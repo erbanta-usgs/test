54db9807f9e64640384a7524659b83c97338090f
Ned Banta <erbanta@usgs.gov>
2015-06-09 16:19:37 -0600

    Minor changes to add some complexity in tests.

--- test4_ts/well-2_rate.ts
+++ test4_ts/well-2_rate.ts
@@ -1,10 +1,11 @@
 # The ATTRIBUTES block is required.
 BEGIN ATTRIBUTES
-  NAME well_2_rate 
-  METHOD linear
+  NAME    well_2_rate  well_3_rate
+  METHOD  linear
+  SFACS   1.0          1.2
 END ATTRIBUTES
-0.0         0.0
-1.0     -2000.0
-9.0    -18000.0
-9.01        2.0
-9.02        0.0
+0.0         0.0        -1000.0
+1.0     -2000.0        -2000.0
+9.0    -18000.0        -3000.0
+9.01        2.0        -4000.0
+9.02        0.0        -5000.0
