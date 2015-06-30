35a6c1037c1cc902bedcf77ce79d5a4eef6be324
Ned Banta <erbanta@usgs.gov>
2015-06-30 17:26:03 -0600

    Added a test that uses 10,000 river nodes controlled by a time series--works as expected. Time-series is read one record at a time with no duplication.

---
+++ mf2015/test_ts_many/riv_stage.ts
@@ -0,0 +1,17 @@
+# The ATTRIBUTES block is required.
+BEGIN ATTRIBUTES
+  NAME river_stage
+  METHOD linear
+END ATTRIBUTES
+   0.0    100.0
+  50.0    101.0
+ 100.0    103.0
+ 150.0    105.0
+ 200.0    104.0
+ 250.0    103.0
+ 300.0    102.0
+ 350.0    101.0
+ 400.0    100.0
+ 450.0     99.0
+ 501.0     95.0
+ 
---
+++ mf2015/test_ts_many/runmodel.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM mf2015d.exe is the Debug executable
+REM ..\..\..\mf2015\bin\mf2015d.exe
+
+REM mf2015.exe is the Release executable
+..\..\..\mf2015\bin\mf2015.exe
+
+pause
