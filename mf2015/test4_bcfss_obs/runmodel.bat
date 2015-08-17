6e094231136ff9adb2c2ad2894bc3fabc9c4422c
Ned Banta <erbanta@usgs.gov>
2015-08-17 11:25:41 -0600

    Eliminated extra (ending) head column in CHD8 input files.

--- mf2015/test4_bcfss_obs/runmodel.bat
+++ mf2015/test4_bcfss_obs/runmodel.bat
@@ -1,3 +1,3 @@
 REM ..\..\msvs\mf2015\debug\mf2015.exe
-..\..\..\mf2015\msvs\mf2015\debug\mf2015.exe
+..\..\..\mf2015\bin\mf2015d.exe
 pause
--- mf2015/test4_tr_obs/runmodel.bat
+++ mf2015/test4_tr_obs/runmodel.bat
@@ -1,2 +1,2 @@
-..\..\..\mf2015\bin\mf2015.exe
+..\..\..\mf2015\bin\mf2015d.exe
 pause
