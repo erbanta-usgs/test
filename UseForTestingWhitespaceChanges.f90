764bd1482551502f817f37acadc86fdbe2b8d45a
Ned Banta <erbanta@usgs.gov>
2015-09-24 18:03:58 -0600

    converted to Old Mac.

--- UseForTestingWhitespaceChanges.f90
+++ UseForTestingWhitespaceChanges.f90
@@ -1,8 +1,1 @@
-! Feel free to edit
-! this file to test
-! whitespace changes.
-! 
-! New line 9/24/2015
-program donothing
-end program donothing
-
+! Feel free to edit! this file to test! whitespace changes.! ! New line 9/24/2015program donothingend program donothing
